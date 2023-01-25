f160fc14bf5bc755acdbeb635bc70be8fe72f45f
Ngô Văn Cảnh <canh25xp@gmail.com>
2023-01-26 02:08:09 +0700

    Update Arithmetic_Logic_Unit.v

--- source code/Arithmetic_Logic_Unit.v
+++ source code/Arithmetic_Logic_Unit.v
@@ -17,11 +17,11 @@ module Arithmetic_Logic_Unit(
     assign ALU_Zflag = ~|ALU_out; //reduction nor
     always @(opcode or data_1 or data_2) begin
         case(opcode)
-            `ADD : ALU_out = data_2 + data_1;
-            `SUB : ALU_out = data_2 - data_1;
-            `AND : ALU_out = data_1 & data_2;
-            `NOT : ALU_out = ~data_2;
-            default : ALU_out = 0;
+            `ADD :      ALU_out = data_2 + data_1;
+            `SUB :      ALU_out = data_2 - data_1;
+            `AND :      ALU_out = data_1 & data_2;
+            `NOT :      ALU_out =        ~ data_2;
+            default :   ALU_out = 0;
         endcase
     end
 endmodule 
@@ -30,7 +30,7 @@ module Arithmetic_Logic_Unit_tb;
     reg [7:0] data_1, data_2;
     reg [3:0] opcode;
     wire[7:0] out;
-    wire      Zflag;
+    wire      Zflag;            
     Arithmetic_Logic_Unit ALU(out, Zflag, data_1, data_2, opcode);
     initial begin
         #0 data_1 = 64; data_2 = 128; opcode = `NOP;
